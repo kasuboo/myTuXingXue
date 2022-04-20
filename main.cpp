27359cc64fb2828ee787ae5d81912e1b202b7e03
kasuboo <80378908+kasuboo@users.noreply.github.com>
2022-04-20 16:18:00 +0800

    纹理check成功

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include<iostream>
+#include<glm.hpp>
+#include<gtc/matrix_transform.hpp>
+#include<gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 using namespace std;
 int main(void)
 {
@@ -30,9 +37,10 @@ int main(void)
 	cout << glGetString(GL_VERSION);
 
 	//定义顶点数据
-	float vertices[6] = {
-		-0.5f,-0.5f,0.0f,
-		0.5f,0.5f,-0.5f
+	float vertices[12] = {
+		-0.5f,-0.5f,0.0f,0.0f, //(-0.5,-0.5)、(0.0,0.0)
+		 0.0f,0.5f,0.5f,1.0f,
+		0.5f,-0.5f,1.0f,0.0f
 	};
 
 	//发送数据
@@ -44,27 +52,66 @@ int main(void)
 
 	//解析数据
 	//配置
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, 0);
 	//配置完成,启用
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)(sizeof(float) * 2));
+	glEnableVertexAttribArray(1);
+
+	//创建纹理
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); //绑定
+	//为绑定设置纹理形式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//翻转图片y轴,使得和opengl方向一致
+	stbi_set_flip_vertically_on_load(true);
+
+	int width, height, nr;
+	unsigned char* data = stbi_load("test.jpg", &width, &height, &nr, 0);
+	cout << width << " " << height << " " << nr << endl;
+	if (data) 
+	{
+		//用载入的图片生成一个纹理
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		cout << "fail to load texture" << endl;
+	}
+	stbi_image_free(data); //释放内存
 
 	//引入着色器
 	//顶点着色器
 	const char* vertexShaderSource =
 		"#version 330 core\n"
 		"layout (location = 0) in vec2 aPos;\n" //设定输入对象的位置值
+		"layout (location=1) in vec2 atexCord;\n" //定义纹理属性作为顶点着色器的输入
+		"out vec2 myTexCord;\n"
+
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
+		    "myTexCord = atexCord;\n" //直接将顶点着色器的纹理坐标输出
 		"}\0";
 
 	//片段着色器
 	const char* fragmentShaderSource =
 		"#version 330 core\n"
 		"out vec4 FragColor;\n"
+		"in vec2 myTexCord;\n"
+		"uniform sampler2D ourTexture;\n"
+
 		"void main()\n"
 		"{\n"
-		"FragColor=vec4(0.0f,0.0f,1.0f,1.0f);\n" //4个元素的数组：红色、绿色、蓝色和alpha(透明度)分量
+		    //"FragColor=vec4(0.0f,0.0f,1.0f,1.0f);\n" //4个元素的数组：红色、绿色、蓝色和alpha(透明度)分量
+		    "FragColor = texture(ourTexture,myTexCord);\n"
 		"}\n";
 	unsigned int vertexShader; //创建一个顶点着色器对象
 	vertexShader = glCreateShader(GL_VERTEX_SHADER); //返回创建出的着色器id
@@ -91,7 +138,7 @@ int main(void)
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT); //清空颜色缓冲
-		glClearColor(1.0, 1.0, 1.0, 0.0); //设置窗口白色
+		//glClearColor(1.0, 1.0, 1.0, 0.0); //设置窗口白色
 		glUseProgram(shaderProgram); //
 		glDrawArrays(GL_TRIANGLES, 0, 3); //绘制模式、索引数组的起始位置、顶点数量
 		glfwSwapBuffers(window);
